dcd0437592b99ebbef42a87595b12dff00a4d43f
RekkyRek <haslun16@edu.vilhelmina.se>
2017-12-28 17:33:07 +0100

    Started working on youtube musicbot

--- plugins.js
+++ plugins.js
@@ -3,13 +3,17 @@ const fs = require('fs')
 function findPlugins() {
 	let plugins = []
 	
+	console.log('Searching for plugins...')
+	
 	fs.readdirSync('./plugins').forEach((plugin)=>{
-		console.log('Searching for plugins...')
 		if(require(`./plugins/${plugin}/plugin.json`)) {
 			const pluginConf = require(`./plugins/${plugin}/plugin.json`)
 			if(!pluginConf.name || !pluginConf.version || !pluginConf.entry) {return}
 			console.log(`Found ${pluginConf.name} ${pluginConf.version}`)
-			plugins.push(require(`./plugins/${plugin}/${pluginConf.entry}`))
+			const tempPlugin = require(`./plugins/${plugin}/${pluginConf.entry}`);
+			tempPlugin.name = pluginConf.name;
+			tempPlugin.version = pluginConf.version;
+			plugins.push(tempPlugin)
 		}
 	})
 	
@@ -18,11 +22,9 @@ function findPlugins() {
 
 function initPlugins(plugins, thot) {
   plugins.forEach((plugin) => {
-  	if(!plugin.name || !plugin.version) {return}
-  	
   	if(plugin.init) {
+  		console.log(`Successfully loaded ${plugin.name} ${plugin.version}.`)
   		plugin.init(thot)
-  		console.log(`Successfully loaded ${plugin.name} ${plugin.version}`)
   	} else {
   		console.warn(`${plugin.name} has no init() Skipping.`)
   	}
---
+++ plugins/youtubeBot/index.js
@@ -0,0 +1,43 @@
+let THOT;
+
+const THOTUtils = require('../../THOTUtils');
+const ytdl = require('ytdl-core');
+
+function play(msg) {
+	let args = THOTUtils.parseParams(msg.content, [""]);
+	if(args.err) { msg.channel.send('Usage: !play <YOUTUBE ID>'); return; }
+	
+	if (msg.member.voiceChannel) {
+		msg.member.voiceChannel.join()
+		.then(connection => { // Connection is an instance of VoiceConnection
+			const stream = ytdl(`http://www.youtube.com/watch?v=${args[0]}`);
+			const dispatcher = connection.playStream(stream);
+			dispatcher.on('end', () => {
+				msg.member.voiceChannel.leave();
+			});
+		})
+		.catch(console.log);
+	} else {
+		msg.reply('You need to join a voice channel first!');
+	}
+}
+
+function begone(msg) {
+	if (msg.member.voiceChannel) {
+		msg.channel.send('no thots :flushed: :flushed: :flushed:');
+		msg.member.voiceChannel.leave();
+	} else {
+		msg.channel.send('but DADDY OwO');
+	}
+}
+
+
+function init(thot) {
+	THOT = thot;
+	THOT.on('play', play);
+	THOT.on('begone', begone);
+}
+
+module.exports = {
+	init: init,
+};
