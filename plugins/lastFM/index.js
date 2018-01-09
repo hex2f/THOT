35453db12745403cbb74dc8d336eff5f12dfe585
RekkyRek <haslun16@edu.vilehlmina.se>
2018-01-09 21:03:58 +0100

    More rich embeds.

--- plugins/lastFM/index.js
+++ plugins/lastFM/index.js
@@ -9,22 +9,19 @@ let api = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=rj
 
 function fmset(msg) {
     let args = THOTUtils.parseParams(msg.content, [""])
-    if(args.err) { msg.channel.send('Usage: !fmset <Username>'); return; }
-    console.log(args[0])
+    if(args.err) { THOT.reply(msg, 'LastFM', 'Usage: !fmset <Username>', 12189696); return; }
     db.push(`/${msg.author.id}`, args[0])
     msg.react('✅');
 }
 
 function currentPlaying(msg) {
-    if(THOT.isDaddy(msg.author)) {
-        console.log('is daddy')
-    }
     try {
         var user = db.getData(`/${msg.author.id}`);
         request(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${user}&api_key=8a3f54b3b37c3a5e0adda40da34af4fb&format=json`, (err, res, body) => {
             if(err) {throw err; return;}
             let data = JSON.parse(body).recenttracks;
-            let str = `**Current**: ${data.track[0].artist['#text']} - ${data.track[0].name}\n**Previous**: ${data.track[1].artist['#text']} - ${data.track[1].name}`;
+            if(data == null) {THOT.reply(msg, 'LastFM', 'Invalid username. Set it using !fmset <Username>', 12189696); return;}
+            let str = `**${data.track[0].artist['#text']} - ${data.track[0].name}**\n${data.track[1].artist['#text']} - ${data.track[1].name}`;
             THOT.reply(msg, `${user}'s LastFM`, str, 12189696);
         });
     } catch(error) {
